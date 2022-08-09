6c57ca4f0c01b9a6df32972b21543c1957e9abc0
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-08 21:15:15 -0300

    setting up project

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT);
