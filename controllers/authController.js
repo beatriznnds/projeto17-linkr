c68cb2c2be692cae1aca3a9685d1ba86e6072ab2
Audrey-Costa <102393980+Audrey-Costa@users.noreply.github.com>
2022-08-09 14:23:16 -0300

    Fix: authController import and requisition
    Remove the unnecessary database import from the controller and put the email search requisition inside the try/catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,16 @@
 import bcrypt from "bcrypt";
 import { authRepository } from "../repositories/authRepository.js";
-import connection from "../database.js";
 
 export async function signUp (req, res) {
     const { email, password, username, profilePic } = req.body;
     const passwordHash = bcrypt.hashSync(password, 10);
-    const { rows: unvalidEmail } = await authRepository.searchByEmail(email);
-    if (unvalidEmail.length !== 0) {
-        return res.status(409).send({ error: 'This e-mail is already registered.'});
-    }
+    
     try {
+        const { rows: unvalidEmail } = await authRepository.searchByEmail(email);
+        if (unvalidEmail.length !== 0) {
+            return res.status(409).send({ error: 'This e-mail is already registered.'});
+        }
+        
         await authRepository.addNewUser(email, passwordHash, username, profilePic);
         res.sendStatus(201);
     } catch (e) {
