16f39c92273e342b91a3d0fc830437f76eb29171
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-10 19:55:41 -0300

    fix: new post schema

--- schemas/newPostSchema.js
+++ schemas/newPostSchema.js
@@ -3,7 +3,8 @@ import Joi from 'joi'
 const newPostSchema = Joi.object({
     link: Joi.string()
         .uri()
-        .required()
+        .required(),
+    description: Joi.string()
 })
 
 export default newPostSchema;
--- tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
-    email varchar(50) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    username varchar(20) NOT NULL UNIQUE,
-    "profilePic" TEXT NOT NULL
-);
