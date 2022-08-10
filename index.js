06460c3f6f210e9ad746800b720e77b8a1822e3f
Audrey Costa <audreycota@nano.ufrj.br>
2022-08-10 17:55:28 -0300

    Fix: Joi schemas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import router from './routes/routes';
+import router from './routes/routes.js';
 
 dotenv.config();
 
