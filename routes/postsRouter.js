15318a1548ed4a4ace7c502f8264b9a10600647c
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-11 17:47:39 -0300

    fix: publish new post

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import schemaValidation from '../middlewares/schemasValidation.js';
 import { newPost } from '../controllers/postsController.js';
 import newPostSchema from '../schemas/newPostSchema.js';
-
+import { validateToken } from '../middlewares/tokenValidation.js';
 const postRouter = Router();
 
-postRouter.post('/publish', schemaValidation(newPostSchema), newPost)
+postRouter.post('/publish', schemaValidation(newPostSchema), validateToken, newPost)
 
 export default postRouter;
