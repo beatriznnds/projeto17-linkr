1eb02aa14b22c8b9c7f63dbaa8baf6264d8af485
Audrey Costa <audreycota@nano.ufrj.br>
2022-08-11 12:24:03 -0300

    Refactor: Joi signin schema validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import schemaValidation from '../middlewares/schemasValidation.js';
 import { signUp, signIn, logout } from '../controllers/authController.js';
 import signUpSchema from '../schemas/signUpSchema.js';
-import userLoginValidationMiddleware from '../middlewares/loginValidation.js';
+import signInSchema from "../schemas/signInSchema.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', schemaValidation(signUpSchema), signUp);
-authRouter.post("/signin", userLoginValidationMiddleware, signIn);
+authRouter.post("/signin", schemaValidation(signInSchema), signIn);
 authRouter.delete("/session", logout);
 
 export default authRouter;
