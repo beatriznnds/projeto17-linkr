2efe5ec35b5337bb1d1716ecaef16e0491557d7d
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-08-11 19:36:21 -0300

    feat: time line controller and router

--- controllers/timelineController.js
+++ controllers/timelineController.js
@@ -1,3 +1,23 @@
 import connection from "../database";
 
-export async function timeline(req, res) {}
+export async function timeline(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer", "").trim();
+  console.log(token);
+
+  const { rows: validToken } = await connection.query(
+    `SELECT * FROM sessions WHERE token = $1`,
+    [token]
+  );
+
+  if (validToken.length === 0) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    await connection.query("SELECT * FROM publications");
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(400);
+  }
+}
