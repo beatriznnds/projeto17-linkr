93473047d2055f113225662a87bdbb9afbd91ab8
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-08-18 20:09:22 -0300

    feat: add  offset

--- repositories/timelineRepository.js
+++ repositories/timelineRepository.js
@@ -1,33 +1,35 @@
 import connection from "../database.js";
 
-async function timeline(){
-    return connection.query(
-        'SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id ORDER BY publications."createdAt" desc LIMIT 20'
-      );
+async function timeline(page) {
+  return connection.query(
+    'SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id ORDER BY publications."createdAt" desc LIMIT 10 OFFSET $1 * 10',
+    [page]
+  );
 }
 
-async function userTimeline(id){
-    return connection.query(
-        'SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id WHERE users.id = $1 ORDER BY publications."createdAt" desc LIMIT 20',
-        [id]
-      );
+async function userTimeline(id) {
+  return connection.query(
+    'SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id WHERE users.id = $1 ORDER BY publications."createdAt" desc LIMIT 20',
+    [id]
+  );
 }
 
-async function hashtagTimeline(hashtag){
-    return connection.query(
-        `SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id WHERE LOWER(description) LIKE $1 ORDER BY publications."createdAt" desc LIMIT 20`, ['%#' + hashtag.toLowerCase() + '%']
-    )
+async function hashtagTimeline(hashtag) {
+  return connection.query(
+    `SELECT publications.*, users."username", users."profilePic" FROM publications JOIN users ON publications."userId" = users.id WHERE LOWER(description) LIKE $1 ORDER BY publications."createdAt" desc LIMIT 20`,
+    ["%#" + hashtag.toLowerCase() + "%"]
+  );
 }
 
-async function trendings(){
-    return connection.query(
-        `SELECT hashtag, COUNT(id) AS "valueOccurrence" FROM hashtags GROUP BY hashtag ORDER BY "valueOccurrence" DESC LIMIT 10`
-    )
+async function trendings() {
+  return connection.query(
+    `SELECT hashtag, COUNT(id) AS "valueOccurrence" FROM hashtags GROUP BY hashtag ORDER BY "valueOccurrence" DESC LIMIT 10`
+  );
 }
 
 export const timelineRepository = {
-    timeline,
-    userTimeline,
-    hashtagTimeline,
-    trendings
-}
+  timeline,
+  userTimeline,
+  hashtagTimeline,
+  trendings,
+};
